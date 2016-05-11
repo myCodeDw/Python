2f64e5ff17c6f9e17736732d22fa56ea96e393c9
nbyue <20671413@163.com>
2016-05-11 17:31:05 +0800

    study5_v1.2

--- 学习5.py
+++ 学习5.py
@@ -1,33 +1,8 @@
-man = []
-other = []
-
-try:
-    data = open('E:\\Python\\chapter3\\sketch.txt')
-    for each_line in data :
-        try:
-            (role,line_spoken) = each_line.split(':',1)
-            line_spoken = line_spoken.strip()
-
-            if role == 'Man':
-                man.append(line_spoken)
-            elif role == 'Other Man':
-                other.append(line_spoken)
-        except ValueError:
-            pass
-
-    data.close()
-
-except IOError:
-    print('The datafile is missing!')
-
-try:
-    man_file = open('man_data.txt',"w")
-    other_file = open('other_data.txt','w')
-
-    print(man,file=man_file)
-    print(other,file=other_file)
-except IOError:
-    print('File error')
+try :
+    data = open('missing.txt')
+    print (data.readline(),end='')
+except IOError as err:
+    print('File error:' + str(err))
 finally:
-    man_file.close()
-    other_file.close()
+    if 'data' in locals():
+        data.close()
